d1c41bfb771edcfec1f16c946c5286cbc9bfc359
Sai Swaroop <saiswaroop874@gmail.com>
2023-02-10 17:22:10 +0300

    c

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+const array = [1,2,3];
+[num1 , , num2 ] = array;
+console.log(num1,num2);
+
+
+const name = {
+      name: "max",
+      age:28
+}
+
+{name} = {name: "max", age:28}
+console.log(name);
